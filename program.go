371b1b6345b2fbb55e77cd5f68567d00eee900df
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-06-18 15:18:01 +0200

    add listAllPrograms method

--- program.go
+++ program.go
@@ -1,38 +1,78 @@
 package sverigesradio
 
+import (
+	"context"
+	"fmt"
+)
+
+const (
+	programEndpoint = "programs"
+)
+
 type ProgramService service
 
-// Program represents
+// Program represents a Sveriges Radio show. Note: different from an episode
 type Program struct {
-	Description     string `json:"description"`
+	Description     string `json:"description,omitempty"`
 	Programcategory struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
+		ID   int    `json:"id,omitempty"`
+		Name string `json:"name,omitempty"`
 	} `json:"programcategory,omitempty"`
-	Email                    string `json:"email"`
-	Phone                    string `json:"phone"`
-	Programurl               string `json:"programurl"`
-	Programslug              string `json:"programslug"`
-	Programimage             string `json:"programimage"`
-	Programimagetemplate     string `json:"programimagetemplate"`
-	Programimagewide         string `json:"programimagewide"`
-	Programimagetemplatewide string `json:"programimagetemplatewide"`
-	Socialimage              string `json:"socialimage"`
-	Socialimagetemplate      string `json:"socialimagetemplate"`
+	Email                    string `json:"email,omitempty"`
+	Phone                    string `json:"phone,omitempty"`
+	Programurl               string `json:"programurl,omitempty"`
+	Programslug              string `json:"programslug,omitempty"`
+	Programimage             string `json:"programimage,omitempty"`
+	Programimagetemplate     string `json:"programimagetemplate,omitempty"`
+	Programimagewide         string `json:"programimagewide,omitempty"`
+	Programimagetemplatewide string `json:"programimagetemplatewide,omitempty"`
+	Socialimage              string `json:"socialimage,omitempty"`
+	Socialimagetemplate      string `json:"socialimagetemplate,omitempty"`
 	Socialmediaplatforms     []struct {
-		Platform    string `json:"platform"`
-		Platformurl string `json:"platformurl"`
-	} `json:"socialmediaplatforms"`
+		Platform    string `json:"platform,omitempty"`
+		Platformurl string `json:"platformurl,omitempty"`
+	} `json:"socialmediaplatforms,omitempty"`
 	Channel struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"channel"`
-	Archived          bool   `json:"archived"`
-	Hasondemand       bool   `json:"hasondemand"`
-	Haspod            bool   `json:"haspod"`
-	Responsibleeditor string `json:"responsibleeditor"`
-	ID                int    `json:"id"`
-	Name              string `json:"name"`
+		ID   int    `json:"id,omitempty"`
+		Name string `json:"name,omitempty"`
+	} `json:"channel,omitempty"`
+	Archived          bool   `json:"archived,omitempty"`
+	Hasondemand       bool   `json:"hasondemand,omitempty"`
+	Haspod            bool   `json:"haspod,omitempty"`
+	Responsibleeditor string `json:"responsibleeditor,omitempty"`
+	ID                int    `json:"id,omitempty"`
+	Name              string `json:"name,omitempty"`
 	Broadcastinfo     string `json:"broadcastinfo,omitempty"`
 	Payoff            string `json:"payoff,omitempty"`
+}
+
+type ProgramOptions struct {
+	ChannelID         *int  `json:"channelid,omitempty"`
+	ProgramCategoryID *int  `json:"programcategoryid,omitempty"`
+	IsArchived        *bool `json:"isarchived,omitempty"`
+}
+
+type ListProgramsResponse struct {
+	Copyright string     `json:"copyright"`
+	Programs  []*Program `json:"programs"`
+}
+
+func (s *ProgramService) ListAllPrograms(ctx context.Context, opt *ProgramOptions) ([]*Program, error) {
+	r, err := addOptions(programEndpoint, opt)
+	if err != nil {
+		return nil, err
+	}
+	r = r + "&format=json&pagination=false"
+	fmt.Printf("Printing options: %v\n", opt)
+
+	req, err := s.client.NewRequest("GET", r, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp *ListProgramsResponse
+	if _, err := s.client.Do(ctx, req, &resp); err != nil {
+		return nil, err
+	}
+	return resp.Programs, nil
 }
