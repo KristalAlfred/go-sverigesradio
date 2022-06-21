c7b3887a0e2c3aa6aa222027b9c9e330fe322ceb
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-06-21 20:48:46 +0200

    add tests

--- sverigesradio/news.go
+++ sverigesradio/news.go
@@ -9,21 +9,21 @@ const programNewsEndpoint = "news"
 
 type NewsService service
 
-func (s *NewsService) GetNewsPrograms(ctx context.Context, opt *GeneralOptions) ([]*Program, error) {
+func (s *NewsService) GetNewsPrograms(ctx context.Context, opt *GeneralOptions) (*ProgramsResponse, error) {
 	r, err := addOptions(programNewsEndpoint, opt)
 	req, err := s.client.NewRequest("GET", r, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var resp *programsResponse
+	var resp *ProgramsResponse
 	if _, err := s.client.Do(ctx, req, &resp); err != nil {
 		return nil, err
 	}
-	return resp.Programs, nil
+	return resp, nil
 }
 
-func (s *NewsService) GetNewsEpisodes(ctx context.Context, opt *GeneralOptions) ([]*Episode, error) {
+func (s *NewsService) GetNewsEpisodes(ctx context.Context, opt *GeneralOptions) (*EpisodesResponse, error) {
 	endpoint := path.Join(programNewsEndpoint, "episodes")
 	r, err := addOptions(endpoint, opt)
 	req, err := s.client.NewRequest("GET", r, nil)
@@ -31,9 +31,9 @@ func (s *NewsService) GetNewsEpisodes(ctx context.Context, opt *GeneralOptions) 
 		return nil, err
 	}
 
-	var resp *episodesResponse
+	var resp *EpisodesResponse
 	if _, err := s.client.Do(ctx, req, &resp); err != nil {
 		return nil, err
 	}
-	return resp.Episodes, nil
+	return resp, nil
 }
--- sverigesradio/news_test.go
+++ sverigesradio/news_test.go
@@ -2,43 +2,30 @@ package sverigesradio
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetNewsPrograms(t *testing.T) {
 	client := NewClient(http.DefaultClient)
-	programs, err := client.News.GetNewsPrograms(context.Background(), &GeneralOptions{
+	resp, err := client.News.GetNewsPrograms(context.Background(), &GeneralOptions{
 		Format: JSON,
 	})
 	if err != nil {
 		t.Errorf("Error occurred in GetProgramCategoryByID(), got error: %v", err)
 	}
 
-	for _, program := range programs {
-		fmt.Println(program.ID)
-	}
-
-	t.Errorf("Heyoo")
-
-	//assert.Equal(t, podfile.Program.Name, "Karlavagnen", "Podfile with ID 4126279 should be from program Karlavagnen")
+	assert.Equal(t, "Ekot", *resp.Programs[0].Name, "The first news show should be 'Ekot'")
 }
 
 func TestGetNewsEpisodess(t *testing.T) {
 	client := NewClient(http.DefaultClient)
-	episodes, err := client.News.GetNewsEpisodes(context.Background(), &GeneralOptions{
+	_, err := client.News.GetNewsEpisodes(context.Background(), &GeneralOptions{
 		Format: JSON,
 	})
 	if err != nil {
 		t.Errorf("Error occurred in GetProgramCategoryByID(), got error: %v", err)
 	}
-
-	for _, episode := range episodes {
-		fmt.Println(episode.ID)
-	}
-
-	t.Errorf("Heyoo")
-
-	//assert.Equal(t, podfile.Program.Name, "Karlavagnen", "Podfile with ID 4126279 should be from program Karlavagnen")
 }
